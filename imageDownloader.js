418e21c03a923e379500e19de698aa02b666cc2c
Aman Kalra <44301957+aman97kalra@users.noreply.github.com>
2020-10-03 13:58:33 +0530

    Added a nodeJs script to download image from a url and write it on disc.
    Added a nodeJs script to download image from a url and write it on disc.

---
+++ imageDownloader.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const https = require('https');
+
+/**
+ * @param {*} url url of the image to be downloaded
+ * @param {*} path path where the image is be saved.
+ */
+function saveImageToDisk( url, path ){
+    var fullUrl = url;
+    var localPath = fs.createWriteStream(path);
+    try{
+        var request = https.get( fullUrl, ( response ) => {
+        //console.log(response);
+        response.pipe(localPath);
+        });
+    } catch( e ) {
+        console.log( 'error', e );
+    }
+};
+
+saveImageToDisk('https://random.dog/vh7i79y2qhhy.jpg',
+'./image.jpg');
